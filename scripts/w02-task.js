b1f414c0ed58634b5493970589cfab3ac6617442
Njninja12 <noah.j.wright316@gmail.com>
2023-09-23 18:31:08 -0700

    edited a bunch of stuff

---
+++ scripts/w02-task.js
@@ -0,0 +1,36 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullname = "Noah Wright";
+let currentYear = new Date().getFullYear();
+let profilePicture = new Image();
+profilePicture.src = "images/Profile Pic.jpg";
+
+
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById("name");
+let foodElement = document.getElementById("food");
+const yearElement = document.querySelector("#year");
+const imageElement = document.getElementById("img");
+
+/* Step 4 - Adding Content */
+nameElement.innerHTML = `<strong>${fullname}</strong>`;
+yearElement.textContent = currentYear
+imageElement.setAttribute("src", profilePicture.src);
+nameElement.setAttribute("alt", `Profile image of ${fullname}`);
+
+
+
+
+/* Step 5 - Array */
+let fav_foods = ["pizza", "wings", "burgers", "chicken alfredo", "orange chicken"];
+let anotherFood = "buffalo chicken";
+fav_foods.push(anotherFood);
+foodElement.innerHTML = fav_foods.join(", ");
+
+
+
+
